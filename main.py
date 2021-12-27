73e70de59107a53aac0bc0ab9ecace6659edbb69
Arthur Pierrot <arthur.pierrot@hotmail.com>
2021-12-27 14:12:36 +0100

    Added parameters

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 
+# Parameters
+# Experiment of fig. 2.
+Z = 200
+Z_r = 40
+Z_p = 160
+c = 0.1
+N = 6
+b = 1
+M = 3*c*b
+b_p = 0.625
+b_r = 2.5
+p_k_max = [x * 10**-3 for x in [2, 40, 75, 3, 2, 20]]
+gradient_k_max = [x * 10**-2 for x in [16, 6, 2, 16, 6, 3]]
+
 
 if __name__ == "__main__":
     pass
