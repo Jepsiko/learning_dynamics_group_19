ea0716a6deb5499ee7b823be87fdf1973e56b07f
Arthur Pierrot <arthur.pierrot@hotmail.com>
2021-12-28 17:42:13 +0100

    T fermi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from math import comb
+from math import comb, e
+from matplotlib import pyplot as plt
 
 # CONSTANTS
 COOPERATOR = "C"
@@ -7,7 +8,7 @@ RICH = "R"
 POOR = "P"
 
 # PARAMETERS
-# Experiment of fig. 2.
+# Eperiment of fig. 2.
 r = 0.2
 Z = 200
 Z_R = 40
@@ -18,19 +19,22 @@ b = 1
 M = 3
 b_P = 0.625
 b_R = 2.5
-p_k_max = [x * 10 ** -3 for x in [2, 40, 75, 3, 2, 20]]
-gradient_k_max = [x * 10 ** -2 for x in [16, 6, 2, 16, 6, 3]]
+p_k_ma = [x * 10 ** -3 for x in [2, 40, 75, 3, 2, 20]]
+gradient_k_ma = [x * 10 ** -2 for x in [16, 6, 2, 16, 6, 3]]
 c_R = c * b_R
 c_P = c * b_P
+beta = 10
+h = 0
+mu = 1/Z
 
 
 def heaviside(k):
     return int(k >= 0)
 
 
-def payoff(j_R, j_P, strategy, wealth):
-    if strategy is DEFECTOR:
-        if wealth is RICH:
+def payoff(j_R, j_P, X, k):
+    if X is DEFECTOR:
+        if k is RICH:
             b_strat = b_R
         else:
             b_strat = b_P
@@ -38,22 +42,22 @@ def payoff(j_R, j_P, strategy, wealth):
         theta = heaviside(c_R*j_R + c_P*j_P - M*c*b)
         return b_strat * (theta + (1 - r) * (1 - theta))
     else:
-        if wealth is RICH:
+        if k is RICH:
             c_strat = c_R
         else:
             c_strat = c_P
-        return payoff(j_R, j_P, DEFECTOR, wealth) - c_strat
+        return payoff(j_R, j_P, DEFECTOR, k) - c_strat
 
 
-def fitness(i_R, i_P, strategy, wealth):
-    if strategy is COOPERATOR:
-        if wealth is RICH:
+def fitness(i_R, i_P, X, k):
+    if X is COOPERATOR:
+        if k is RICH:
             a, b, x, m, n = -1, 0, 0, 1, 0
         else:
-            a, b, x, m, n = 0, 0, -1, 0, 0
-    else:
-        if wealth is RICH:
             a, b, x, m, n = 0, -1, 0, 0, 1
+    else:
+        if k is RICH:
+            a, b, x, m, n = 0, 0, -1, 0, 0
         else:
             a, b, x, m, n = 0, 0, -1, 0, 0
 
@@ -62,10 +66,89 @@ def fitness(i_R, i_P, strategy, wealth):
         for j_P in range(N - j_R):
             mul = comb(i_R + a, j_R) * comb(i_P + b, j_P)
             mul *= comb(Z + x - i_R - i_P, N - 1 - j_R - j_P)
-            mul *= payoff(j_R + m, j_P + n, strategy, wealth)
+            mul *= payoff(j_R + m, j_P + n, X, k)
             sum_ += mul
     return comb(Z - 1, N - 1)**-1 * sum_
 
 
+def fermi(i_R, i_P, start_X, end_X, start_k, end_k):
+    return 1 / (1 + e**(beta*(fitness(i_R, i_P, start_X, start_k) - fitness(i_R, i_P, end_X, end_k))))
+
+
+def T(i_R, i_P, X, Y, k):
+    if k is RICH:
+        Z_k = Z_R
+        Z_l = Z_P
+        l = POOR
+    else:
+        Z_k = Z_P
+        Z_l = Z_R
+        l = RICH
+
+    if X is COOPERATOR:
+        if k is RICH:
+            i_X_k = i_R
+        else:
+            i_X_k = i_P
+    else:
+        if k is RICH:
+            i_X_k = Z_R - i_R
+        else:
+            i_X_k = Z_P - i_P
+
+    if Y is COOPERATOR:
+        if k is RICH:
+            i_Y_k = i_R
+            i_Y_l = i_P
+        else:
+            i_Y_k = i_P
+            i_Y_l = i_R
+    else:
+        if k is RICH:
+            i_Y_k = Z_R - i_R
+            i_Y_l = Z_P - i_P
+        else:
+            i_Y_k = Z_P - i_P
+            i_Y_l = Z_R - i_R
+
+    left_term = i_Y_k / ((Z_k - 1 + (1 - h) * Z_l) * (1 + fermi(i_R, i_P, X, Y, k, k)))
+    right_term = (1 - h) * i_Y_l / ((Z_k - 1 + (1 - h) * Z_l) * (1 + fermi(i_R, i_P, X, Y, k, l)))
+    return i_X_k / Z * ((1 - mu) * (left_term + right_term) + mu)
+
+
+def V(i_R, i_P):
+    return i_R * Z_P + i_P
+
+
+def V_minus_1(x):
+    return x // Z_P, x % Z_P
+
+
+def create_transition_matrix(X, Y):
+    F = [[0 for _ in range(Z_R)] for _ in range(Z_P)]
+    for i_P in range(Z_P):
+        for i_R in range(Z_R):
+            try:
+                F[i_P][i_R] = T(i_R, i_P, X, Y, POOR)
+            except ValueError:
+                F[i_P][i_R] = 0
+    return F
+
+
 if __name__ == "__main__":
-    pass
+    X, Y = DEFECTOR, DEFECTOR
+    F = create_transition_matrix(X, Y)
+    plt.matshow(F)
+
+    X, Y = DEFECTOR, COOPERATOR
+    F = create_transition_matrix(X, Y)
+    plt.matshow(F)
+
+    X, Y = COOPERATOR, DEFECTOR
+    F = create_transition_matrix(X, Y)
+    plt.matshow(F)
+
+    X, Y = COOPERATOR, COOPERATOR
+    F = create_transition_matrix(X, Y)
+    plt.matshow(F)
+    plt.show()
